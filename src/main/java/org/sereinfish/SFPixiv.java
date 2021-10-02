cfb82aab73e840990cac2df8a39dc6fd7146c3bf
coide-SaltedFish <15368280291l@gmail.com>
2021-10-02 14:28:21 +0800

    可指定api

--- src/main/java/org/sereinfish/SFPixiv.java
+++ src/main/java/org/sereinfish/SFPixiv.java
@@ -1,17 +1,19 @@
 package org.sereinfish;
 
+import lombok.Setter;
 import org.sereinfish.entity.Pixiv;
 
 import java.io.IOException;
 
+@Setter
 public class SFPixiv {
+    public static String api = "https://api.obfs.dev/api/pixiv/illust";
     //获取图片信息
     /**
      * 得到图片信息
      * @return
      */
     public static Pixiv getIllust(long uid) throws IOException {
-        String api = "https://api.obfs.dev/api/pixiv/illust";
         String json = SFUtil.getStr(api, "id=" + uid);
         Pixiv pixivEntity = SFUtil.toClass(json, Pixiv.class);
 
